11f9ec99b7cec33a797427d0ff2424c096217619
Eric Sohn <esohn@ool-ad0205ca.dyn.optonline.net>
2019-02-04 16:11:50 -0500

    new pictures added and removed old pictures, created function to create goblins outside of window and another function to move them down

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const desc = document.querySelector('.description');
 const button = document.querySelector('button');
 const input = document.querySelector('input');
 const form = document.querySelector('form');
-const headerOne = document.querySelector('.headerOne');
-const headerTwo = document.querySelector('.headerTwo');
+const headerOne = document.querySelector('.header-one');
+const headerTwo = document.querySelector('.header-two');
 const header = document.querySelector('header');
 let numberGoblins = 0;
 let castleHealth = 5;
 
+//Creates second page with instructions and removes input and first button after submitting the name
 function createSecondPage(evg){
   evg.preventDefault();
   const playName = input.value;
@@ -15,7 +16,8 @@ function createSecondPage(evg){
   input.remove();
   button.remove();
 }
-//Creates second button to play the game after submitting name
+
+//Creates start button to play the game after submitting name
 function createButtonTwo(){
   const buttonTwo = document.createElement('button');
   buttonTwo.setAttribute('class', 'second-button');
@@ -30,15 +32,31 @@ button.addEventListener('click', (ev) => {
   createButtonTwo();
 })
 
+//positions goblin at random position on the top of page
+function randomPos(goblinObjects){
+  goblinObjects.style.top = '-20px';
+  goblinObjects.style.left = Math.random() * window.innerWidth + 'px';
+}
+
 //creates goblin with function it wants to stop as parameter
 function createGoblin(bc){
     let goblins = document.createElement('div');
     goblins.setAttribute('class', 'green-goblins');
-    document.body.appendChild(goblins);
     randomPos(goblins);
+    document.body.appendChild(goblins);
+
+    setInterval(() => {moveGoblin(goblins)}, 100);
+
     numberGoblins += 1;
-    checkHowMany(5, bc);
+    checkHowMany(20, bc);
 }
+
+function moveGoblin(goblinOb){
+  goblinOb.style.top = '500px';
+}
+
+// setInterval(() => {goblins.classList.toggle('walk')}, 250);
+
 
 //checks how many number of goblins and stops running function when it reaches limit
 function checkHowMany(limit, ab){
@@ -47,29 +65,22 @@ function checkHowMany(limit, ab){
   }
 }
 
-function addHealthScore(){
-  let healthScore = document.createElement('div');
-  healthScore.classList.add('health');
-  healthScore.innerHTML = `Health: ${castleHealth}`;
-  header.appendChild(healthScore);
-  header.style.fontSize = '50px';
-  header.style.textAlign = 'left';
-  header.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-  header.style.color = '#fff';
-  header.style.padding = '20px';
-}
-
-// function randomPos(goblinObjects){
-//     goblinObjects.style.top = window.innerHeight + 'px';
-//     goblinObjects.style.left = Math.random() * window.innerWidth + 'px';
-//   }
-
 //starts the game every time start button is pressed
 function startGame(ev){
   ev.preventDefault();
   headerOne.remove();
   headerTwo.remove();
   addHealthScore();
+
   let goblinAppear = setInterval(() => {
-    createGoblin(goblinAppear)}, 1000);
+  createGoblin(goblinAppear)}, 1000);
+}
+
+//displays the health score on upper right side
+function addHealthScore(){
+  let healthScore = document.createElement('div');
+  healthScore.classList.add('health');
+  healthScore.innerHTML = `Health: ${castleHealth}`;
+  header.appendChild(healthScore);
+  header.style.backgroundColor = 'rgba(0, 0, 0, 0)';
 }
